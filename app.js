933318639e02d00cc591648e4682c133a7e51ce1
Some_one <hadassafavari48@gmail.com>
2025-12-18 15:53:14 -0300

    vamo lá

--- app.js
+++ app.js
@@ -1,69 +1,22 @@
-function getText(number) {
-    return citacoes[number].text;
-}
-
-function getAuthor(number) {
-    return citacoes[number].author;
-}
+const quotes = require('./quotes.js');
+const random = require('random');
 
 function getNumber(lastNumber){
-    if (lastNumber < 9){
-        return lastNumber+1;
+    let newNumber = random.int((min = 0), (max = 9));
+    if (newNumber == lastNumber) {
+        lastNumber = newNumber;
+        getNumber();
     }else {
-        return 0;
+        return newNumber;
     }
 }
 
-const citacoes = [
-    c0 = {
-        text: "Não é sobre você começa, mas sobre como você termina",
-        author: "Michael Phelps"
-    },
-    c1 = {
-        text: "O ser humano deve desenvolver, para todos os seus conflitos, um método que rejeite a vingança, a agressão e a retaliação. A base para esse tipo de método é o amor",
-        author: "Martin Luther King Jr."
-    },
-    c2 = {
-        text: "É impossível haver progresso sem mudanças, e aqueles que não conseguem mudar suas mentes nada mudam",
-        author: "George Bernard Shaw"
-    },
-    c3 = {
-        text: "Não se enganem. Uma gotinha no oceano faz, sim, muita diferença",
-        author: "Zilda Arns"
-    },
-    c4 = {
-        text: "O objetivo da educação é o avanço do conhecimento e a disseminação da verdade",
-        author: "John F. Kennedy"
-    },
-    c5 = {
-        text: "É importante não deixar de questionar. A curiosidade tem uma razão de existir",
-        author: "Albert Eistein"
-    },
-    c6 = {
-        text: "O homem é por natureza um animal político",
-        author: "Aristóteles"
-    },
-    c7 = {
-        text: "O homem está condenado a ser livre; porque uma vez jogado ao mundo, ele é responsável por tudo que faz",
-        author: "Jean-Paul Sartre"
-    },
-    c8 = {
-        text: "O coração tem razões que a própria razão desconhece",
-        author: "Blaise Pascal"
-    },
-    c9 = {
-        text: "Uma boa ideia é algo que não resolve apenas um único problema, mas sim pode resolver vários problemas de uma só vez",
-        author: "Shigeru Miyamoto"
-    }
-];
+const citacoes = quotes.getJustQuote();
+const autores = quotes.getJustAuthor();
 
 const quoteText = document.querySelector('.citacao');
 const quoteAuthor = document.querySelector('.autor');
 const newQuote = document.querySelector('#new-quote-button');
-
-let ln = 0;
-quoteText.textContent = getText(ln);
-    quoteAuthor.textContent = getAuthor(ln);
 
 newQuote.addEventListener('click', () =>{
     Toastify({
@@ -83,10 +36,15 @@ newQuote.addEventListener('click', () =>{
         //onClick: function(){} // Callback after click
     }).showToast();
 
-    setTimeout(() => {
-        let n = getNumber(ln);
-        ln = n;
-        quoteText.textContent = getText(n);
-        quoteAuthor.textContent = getAuthor(n);
-    }, 1500);
+    function fetchRandomQuote(){
+        return new Promise;
+    }
+
+    Promise = () => {
+        setTimeout(() => {
+            let n = getNumber(ln);
+            quoteText.textContent = getText(cite,n);
+            quoteAuthor.textContent = getAuthor(cite,n);
+        }, 1500);
+    };
 });
--- quotes.js
+++ quotes.js
@@ -1,51 +1,99 @@
-const citacoes = [
-    c0 = {
-        text: "Não é sobre você começa, mas sobre como você termina",
-        author: "Michael Phelps"
-    },
-    c1 = {
-        text: "O ser humano deve desenvolver, para todos os seus conflitos, um método que rejeite a vingança, a agressão e a retaliação. A base para esse tipo de método é o amor",
-        author: "Martin Luther King Jr."
-    },
-    c2 = {
-        text: "É impossível haver progresso sem mudanças, e aqueles que não conseguem mudar suas mentes nada mudam",
-        author: "George Bernard Shaw"
-    },
-    c3 = {
-        text: "Não se enganem. Uma gotinha no oceano faz, sim, muita diferença",
-        author: "Zilda Arns"
-    },
-    c4 = {
-        text: "O objetivo da educação é o avanço do conhecimento e a disseminação da verdade",
-        author: "John F. Kennedy"
-    },
-    c5 = {
-        text: "É importante não deixar de questionar. A curiosidade tem uma razão de existir",
-        author: "Albert Eistein"
-    },
-    c6 = {
-        text: "O homem é por natureza um animal político",
-        author: "Aristóteles"
-    },
-    c7 = {
-        text: "O homem está condenado a ser livre; porque uma vez jogado ao mundo, ele é responsável por tudo que faz",
-        author: "Jean-Paul Sartre"
-    },
-    c8 = {
-        text: "O coração tem razões que a própria razão desconhece",
-        author: "Blaise Pascal"
-    },
-    c9 = {
-        text: "Uma boa ideia é algo que não resolve apenas um único problema, mas sim pode resolver vários problemas de uma só vez",
-        author: "Shigeru Miyamoto"
-    }
-];
-
-let newCitacoes = [];
-for (let i = 0; i < 10; i++) {
-    let write = JSON.stringify(citacoes[i]);
-    newCitacoes.push(write);
-    fs.writeFileSync('quotes.json', write);
+function getQuotesComplex() {
+    const citacoes = [
+        c0 = {
+            text: "Não é sobre você começa, mas sobre como você termina",
+            author: "Michael Phelps"
+        },
+        c1 = {
+            text: "O ser humano deve desenvolver, para todos os seus conflitos, um método que rejeite a vingança, a agressão e a retaliação. A base para esse tipo de método é o amor",
+            author: "Martin Luther King Jr."
+        },
+        c2 = {
+            text: "É impossível haver progresso sem mudanças, e aqueles que não conseguem mudar suas mentes nada mudam",
+            author: "George Bernard Shaw"
+        },
+        c3 = {
+            text: "Não se enganem. Uma gotinha no oceano faz, sim, muita diferença",
+            author: "Zilda Arns"
+        },
+        c4 = {
+            text: "O objetivo da educação é o avanço do conhecimento e a disseminação da verdade",
+            author: "John F. Kennedy"
+        },
+        c5 = {
+            text: "É importante não deixar de questionar. A curiosidade tem uma razão de existir",
+            author: "Albert Eistein"
+        },
+        c6 = {
+            text: "O homem é por natureza um animal político",
+            author: "Aristóteles"
+        },
+        c7 = {
+            text: "O homem está condenado a ser livre; porque uma vez jogado ao mundo, ele é responsável por tudo que faz",
+            author: "Jean-Paul Sartre"
+        },
+        c8 = {
+            text: "O coração tem razões que a própria razão desconhece",
+            author: "Blaise Pascal"
+        },
+        c9 = {
+            text: "Uma boa ideia é algo que não resolve apenas um único problema, mas sim pode resolver vários problemas de uma só vez",
+            author: "Shigeru Miyamoto"
+        }
+    ];
+    return citacoes;
 }
 
-//fs.writeFileSync('quotes.json', newCitacoes);
+function getTextComplex(quote, number) {
+    return quote[number].text;
+}
+
+function getAuthorComplex(quote, number) {
+    return quote[number].author;
+}
+
+function getJustQuote(){
+    const citacoes = [
+        "Não é sobre você começa, mas sobre como você termina",
+        "O ser humano deve desenvolver, para todos os seus conflitos, um método que rejeite a vingança, a agressão e a retaliação. A base para esse tipo de método é o amor",
+        "É impossível haver progresso sem mudanças, e aqueles que não conseguem mudar suas mentes nada mudam",
+        "Não se enganem. Uma gotinha no oceano faz, sim, muita diferença",
+        "O objetivo da educação é o avanço do conhecimento e a disseminação da verdade",
+        "É importante não deixar de questionar. A curiosidade tem uma razão de existir",
+        "O homem é por natureza um animal político",
+        "O homem está condenado a ser livre; porque uma vez jogado ao mundo, ele é responsável por tudo que faz",
+        "O coração tem razões que a própria razão desconhece",
+        "Uma boa ideia é algo que não resolve apenas um único problema, mas sim pode resolver vários problemas de uma só vez",
+    ];
+
+    return citacoes;
+}
+
+function getJustAuthor() {
+    const author = [
+        "Michael Phelps",
+        "Martin Luther King Jr.",
+        "George Bernard Shaw",
+        "Zilda Arns",
+        "John F. Kennedy",
+        "Albert Eistein",
+        "Aristóteles",
+        "Jean-Paul Sartre",
+        "Blaise Pascal",
+        "Shigeru Miyamoto"
+    ];
+
+    return author;
+}
+
+function getAuthorComplex(quote, number) {
+    return quote[number].author;
+}
+
+module.exports = {
+    getQuotesComplex,
+    getTextComplex, 
+    getAuthorComplex,
+    getJustQuote,
+    getJustAuthor
+};
