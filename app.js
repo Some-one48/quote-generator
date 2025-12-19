54cf6f4a904268cff9a9e4e0aa91c7553ea7e549
Some_one <hadassafavari48@gmail.com>
2025-12-19 10:51:44 -0300

    satisfeito, agr?

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const quoteText = document.querySelector('.citacao');
 const quoteAuthor = document.querySelector('.autor');
 const newQuote = document.querySelector('#new-quote-button');
 
-function randomQuote(delay, n){
+function randomQuote(){
+    let delay = chance.natural({min: 1000, max: 2000});
+        let n = chance.natural({min: 0, max:9});
+
     return new Promise(resolver => {
         setTimeout(() => {
             resolver (citacoes[n]);
@@ -10,19 +13,9 @@ function randomQuote(delay, n){
     });
 }
 
-function randomAuthor(delay, n){
-    return new Promise(resolverA => {
-        setTimeout(() => {
-            resolverA (authores[n]);
-        }, delay);
-    });
-}
-
 newQuote.addEventListener('click', async () =>{
     try {
-        let delay = chance.natural({min: 1000, max: 2000});
-        let n = chance.natural({min: 0, max:9});
-
+        
         Toastify({
             text: "Buscando uma nova citação... ",
             duration: 2000,
@@ -38,10 +31,10 @@ newQuote.addEventListener('click', async () =>{
         }).showToast();
 
         const quote = await randomQuote(delay, n);
-        const author = await randomAuthor(delay, n);
+        //const author = await randomAuthor(delay, n);
 
         quoteText.textContent = quote;
-        quoteAuthor.textContent = author;
+        //quoteAuthor.textContent = author;
 
         gsap.fromTo(".quote-container", {
                 x: -40,
