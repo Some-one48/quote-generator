9f4caff41da17cd5ad969300c612d4759344983e
Some_one <hadassafavari48@gmail.com>
2025-12-19 10:47:50 -0300

    acho que agora sim

--- app.js
+++ app.js
@@ -2,12 +2,48 @@ const quoteText = document.querySelector('.citacao');
 const quoteAuthor = document.querySelector('.autor');
 const newQuote = document.querySelector('#new-quote-button');
 
-function resolver(){
-    let n = chance.natural({min: 0, max:9});
-            quoteText.textContent = citacoes[n];
-            quoteAuthor.textContent = authores[n];
+function randomQuote(delay, n){
+    return new Promise(resolver => {
+        setTimeout(() => {
+            resolver (citacoes[n]);
+        }, delay);
+    });
+}
 
-            gsap.fromTo(".quote-container", {
+function randomAuthor(delay, n){
+    return new Promise(resolverA => {
+        setTimeout(() => {
+            resolverA (authores[n]);
+        }, delay);
+    });
+}
+
+newQuote.addEventListener('click', async () =>{
+    try {
+        let delay = chance.natural({min: 1000, max: 2000});
+        let n = chance.natural({min: 0, max:9});
+
+        Toastify({
+            text: "Buscando uma nova citação... ",
+            duration: 2000,
+            newWindow: true,
+            close: true,
+            gravity: "top", 
+            position: "right", 
+            stopOnFocus: true, 
+            style: {
+                background: "#aaaeaf",
+                color: "#190202",
+            },
+        }).showToast();
+
+        const quote = await randomQuote(delay, n);
+        const author = await randomAuthor(delay, n);
+
+        quoteText.textContent = quote;
+        quoteAuthor.textContent = author;
+
+        gsap.fromTo(".quote-container", {
                 x: -40,
                 opacity: 0,
             }, {
@@ -15,36 +51,6 @@ function resolver(){
                 opacity: 100,
                 duration: 1,
             });
-}
-
-function fetchRandomQuote(){
-    let delay = chance.natural({min: 1000, max: 2000});
-
-    return new Promise((resolve) => {
-        resolve(setTimeout(() => {resolver()}, delay));
-    });
-}
-
-newQuote.addEventListener('click', async () =>{
-    try {
-        Toastify({
-            text: "Buscando uma nova citação... ",
-            duration: 2000,
-            //destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "right", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "#aaaeaf",
-                color: "#190202",
-                //"linear-gradient(to right, #97123a, #aaaeaf, #97123a)",
-            },
-            //onClick: function(){} // Callback after click
-        }).showToast();
-
-        const promise = await fetchRandomQuote();
     } catch (error) {
         console.log("ERRO:", error);
     }
